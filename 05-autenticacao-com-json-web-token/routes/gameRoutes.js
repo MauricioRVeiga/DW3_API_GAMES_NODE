676706e1f01ce0f36c5e9ea7026546fd81306667
DSM3 <fatec-dsm3@academico.intra>
2025-09-09 20:20:29 -0300

    quinta aula

--- 05-autenticacao-com-json-web-token/routes/gameRoutes.js
+++ 05-autenticacao-com-json-web-token/routes/gameRoutes.js
@@ -1,21 +1,23 @@
 import express from "express";
 const gameRoutes = express.Router();
 import gameController from "../controllers/gameController.js";
+// Importando o middleware 
+import Auth from "../middlewares/Auth.js";
 
 // a camada de routes será responsável por definir as rotas da aplicação
 // ENDPOINT PARA LISTASR OS JOGOS
 gameRoutes.get("/games", gameController.getAllGames);
 
 // ENDPOINT PARA CADASTRAR OS JOGOS
-gameRoutes.post("/games", gameController.createGame);
+gameRoutes.post("/games", Auth.Authorization ,gameController.createGame);
 
 //// ENDPOINT PARA ALTERAR JOGOS// ENDPOINT PARA ALTERAR JOGOS
-gameRoutes.put("/games/:id", gameController.updateGame);
+gameRoutes.put("/games/:id", Auth.Authorization ,gameController.updateGame);
 
 /// ENDPOINT PARA LISTAR UM UNICO JOGO
-gameRoutes.get("/games/:id", gameController.getOneGame);
+gameRoutes.get("/games/:id", Auth.Authorization ,gameController.getOneGame);
 
 // ENDPOINT PARA DELETAR OS JOGOS
-gameRoutes.delete("/games/:id", gameController.deleteGame);
+gameRoutes.delete("/games/:id", Auth.Authorization ,gameController.deleteGame);
 
 export default gameRoutes;
